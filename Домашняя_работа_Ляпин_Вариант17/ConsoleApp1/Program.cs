833fabe76ce961ffa00b6a2db5b410d15ba3c17b
Максим Ляпин <max.curnosow2015@yandex.ru>
2021-09-13 19:29:20 +0300

    Задание выполнил

--- Домашняя_работа_Ляпин_Вариант17/ConsoleApp1/Program.cs
+++ Домашняя_работа_Ляпин_Вариант17/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// вариант 17
+using System;
+
 
 namespace ConsoleApp1
 {
@@ -6,7 +8,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the temperature in Celsius: ");
+            string numberText = Console.ReadLine();
+            double number = double.Parse(numberText);
+            double result = ((number * 9 / 5) + 32);
+            Console.WriteLine($"Temperature in Fahrenheit = {result}");
+
+            Console.ReadKey(true);
         }
     }
 }
